7ca199cc89160b175b4550e69b5841827b718d1b
Mustafa Alsalfiti <mmsalfiti@gmail.com>
2022-03-17 10:26:16 +0000

    fix:fix paths

--- setup.sh
+++ setup.sh
@@ -1,11 +1,11 @@
 if [[ "$NETWORK_SIZE" == "normal" ]]
 then
-    cd ./network
+    cd $GITHUB_PATH/network
     sh ./all.sh up -d
     sh ./initNodes.sh
 elif [[ "$NETWORK_SIZE" == "small" ]]
 then
-    cd ./network-small
+    cd $GITHUB_PATH/network-small
     sh ./all.sh up -d
     sh ./initNodes.sh
 fi
