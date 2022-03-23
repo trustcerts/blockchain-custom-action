b49126f5000178b0a1e6fe397604417ffb6ffddd
Mustafa Alsalfiti <mmsalfiti@gmail.com>
2022-03-23 16:20:12 +0000

    fix:update folder name

--- cleanup.js
+++ cleanup.js
@@ -7,19 +7,19 @@ const { run } = require('./shared');
 const main = async () => {
     const containers_names = await run("docker ps --format '{{.Names}}'");
 
-    // check if folder docker-logs exists if not create one if yes then clean it
-    const dir = `${__dirname}/docker-logs`;
+    // check if folder docker_logs exists if not create one if yes then clean it
+    const dir = `${__dirname}/docker_logs`;
     if(existsSync(dir)){
         rmSync(dir, { recursive: true, force: true });
     };
     mkdirSync(dir);
     containers_names.forEach(async (name)=> {
-        await run(`docker logs ${name} > docker-logs/${name}.$LOG_FORMAT`)
+        await run(`docker logs ${name} > docker_logs/${name}.$LOG_FORMAT`)
     });
 
     const artifactClient = artifact.create()
-    const artifactName = 'docker-logs';
-    const path = `${__dirname}/docker-logs`
+    const artifactName = 'docker_logs';
+    const path = `${__dirname}/docker_logs`
     const files = readdirSync(path)
     const options = {
         continueOnError: true
